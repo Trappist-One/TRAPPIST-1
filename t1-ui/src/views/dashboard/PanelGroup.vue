39dea89d02bfe20e30838c3119e776ef060eb44c
trappistOne <3185641236@qq.com>
2022-03-30 00:11:46 +0800

    图片更改

--- t1-ui/src/views/dashboard/PanelGroup.vue
+++ t1-ui/src/views/dashboard/PanelGroup.vue
@@ -39,32 +39,32 @@
         </div>
       </div>
     </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('shoppings')">
-        <div class="card-panel-icon-wrapper icon-shopping">
-          <svg-icon icon-class="shopping" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            周访问量
-          </div>
-          <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />
-        </div>
-      </div>
-    </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('shoppings')">
-        <div class="card-panel-icon-wrapper icon-shopping">
-          <svg-icon icon-class="shopping" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            月访问量
-          </div>
-          <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />
-        </div>
-      </div>
-    </el-col>
+<!--    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">-->
+<!--      <div class="card-panel" @click="handleSetLineChartData('shoppings')">-->
+<!--        <div class="card-panel-icon-wrapper icon-shopping">-->
+<!--          <svg-icon icon-class="shopping" class-name="card-panel-icon" />-->
+<!--        </div>-->
+<!--        <div class="card-panel-description">-->
+<!--          <div class="card-panel-text">-->
+<!--            周访问量-->
+<!--          </div>-->
+<!--          <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </el-col>-->
+<!--    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">-->
+<!--      <div class="card-panel" @click="handleSetLineChartData('shoppings')">-->
+<!--        <div class="card-panel-icon-wrapper icon-shopping">-->
+<!--          <svg-icon icon-class="shopping" class-name="card-panel-icon" />-->
+<!--        </div>-->
+<!--        <div class="card-panel-description">-->
+<!--          <div class="card-panel-text">-->
+<!--            月访问量-->
+<!--          </div>-->
+<!--          <count-to :start-val="0" :end-val="13600" :duration="3600" class="card-panel-num" />-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </el-col>-->
   </el-row>
 </template>
 
